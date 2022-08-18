58ec0ca18ae028133ebd001735ff739744142f6b
Zarul Zakuan <zarulzakuan@gmail.com>
2022-08-18 23:04:06 +0800

    fixed bugs

--- gostan.go
+++ gostan.go
@@ -9,163 +9,104 @@
 package gostan
 
 import (
-	"bytes"
 	"io"
 	"os"
 )
 
 // Default max buffer lenght is 8kb
-var MAX_LENGTH int64 = 8192
-
-// SetBufferSize sets
-func SetBufferSize(len int64) {
-	MAX_LENGTH = len
-}
+const MAX_LENGTH int64 = 8192
 
 func SetNewlineSeperator(sep []byte) {
 	// TODO
 }
 
 // ReadFromEnd Move file descriptor current position to the end and start reading backward
-func ReadFromEnd(fr *os.File, out *io.PipeWriter) {
+func ReadFromEnd(out *io.PipeWriter, file_descriptors ...*os.File) {
+	delim_char := byte('\n')
 	defer out.Close()
-	readBuffer := make([]byte, MAX_LENGTH)    // for read buffer, cap == len
-	tempBuffer := make([]byte, 0, MAX_LENGTH) // for temp buffer, it has to be dynamic depends on the size of remainder chars to be carry forward to the next buffer window
-	outputBuffer := make([]byte, 0)           // use this buffer to store 1 row at a time to be piped out to the next processor
+	for i, _ := range file_descriptors {
+		filefile_descriptor := file_descriptors[len(file_descriptors)-1-i]
+		readBuffer := [MAX_LENGTH]byte{}
+		tempBuffer := make([]byte, 0)   // for temp buffer, it has to be dynamic depends on the size of remainder chars to be carry forward to the next buffer window
+		outputBuffer := make([]byte, 0) // use this buffer to store 1 row at a time to be piped out to the next processor
 
-	readBufferLen := MAX_LENGTH
-	var firstRead bool = true
-	var firstOutput bool = true
+		// find file offset position from the beginning of the file
+		file_size, _ := filefile_descriptor.Seek(0, io.SeekEnd) // or cursor position
+		file_pos := file_size
 
-	// find file offset position from the beginning of the file
-	file_size, _ := fr.Seek(0, io.SeekEnd) // or cursor position
-	file_pos := file_size
+		first_scan := true
 
-	// start reading file a buffer at a time
-	for {
-		file_pos -= readBufferLen // on file, go back readBufferLen at a time
+		// start reading file a buffer at a time
+		for {
+			readBufferLen := MAX_LENGTH
+			file_pos -= readBufferLen // on file, go back readBufferLen at a time
 
-		if firstRead {
-			// this means file is smaller than our read buffer,
+			// read byte from current cursor (file_pos), this cursor moves back readBufferLen at a time
+			filefile_descriptor.ReadAt(readBuffer[:], file_pos)
+
+			// initial index always at the back of readbuffer
+			var string_start_index int64 = readBufferLen - 1
+			var string_end_index int64 = readBufferLen - 1
+			// var string_next_end_index int64 = readBufferLen - 1
+
+			var delim_index int64 = -1
+
 			if file_pos < 0 {
-				// so we need to make the readbuffer to the beginning of the file == 0
-				file_pos = 0
-			}
-		}
+				if file_pos < -readBufferLen {
+					out.Write(tempBuffer[0:])
+					break
+				}
+				filefile_descriptor.ReadAt(readBuffer[:readBufferLen+file_pos], 0)
 
-		// if ahead of the starting of the file, break
-		if file_pos < 0 {
-			//write the remaining stuff in our temp buffer if any
-
-			if len(tempBuffer) > 0 {
-				fr.ReadAt(readBuffer[0:MAX_LENGTH+file_pos], 0)
-				tempBuffer = append(readBuffer[0:MAX_LENGTH+file_pos], tempBuffer...)
-				out.Write(tempBuffer[0:])
-			}
-			// there are still some left in the file so we write them out as well
-			// if file_pos > -MAX_LENGTH {
-			// 	fr.ReadAt(readBuffer[0:MAX_LENGTH+file_pos], 0)
-			// 	out.Write(readBuffer[0 : MAX_LENGTH+file_pos])
-			// }
-			// and that's the end of file read
-			break
-		}
-
-		// read byte from current cursor (file_pos), this cursor moves back readBufferLen at a time
-		fr.ReadAt(readBuffer[:], file_pos)
-
-		// for first read, append newline at the rightmost of readbuffer, so need to offset by 1 to make room
-		if firstRead {
-			fr.ReadAt(readBuffer[0:], file_pos+1)
-			readBuffer[readBufferLen-1] = byte('\n')
-			// reset cursor
-			file_pos += 1
-		}
-
-		string_start_pos := 0
-		string_end_pos := 0
-		string_next_end_pos := 0
-
-		// scan backward
-		for i := range readBuffer {
-			wasOutput := false
-			searcher_pos := int(readBufferLen) - 1 - i
-			string_end_pos = string_next_end_pos
-
-			// start of scan
-			if searcher_pos == int(readBufferLen)-1 {
-				string_end_pos = int(readBufferLen) // always at the length of buffer
-				string_next_end_pos = string_end_pos
 			}
 
-			// found delim
-			if readBuffer[searcher_pos] == '\n' {
-				string_start_pos = searcher_pos + 1
-				string_next_end_pos = string_start_pos
-				if string_start_pos > int(readBufferLen)-1 {
-					// ignore
-					outputBuffer = append(outputBuffer, readBuffer[string_start_pos:string_end_pos]...)
-					outputBuffer = append(outputBuffer, tempBuffer[0:]...)
-					tempBuffer = tempBuffer[:0]
+			// scan backward. Remember, we're working in a read buffer now!
+			for i := range readBuffer {
+				scanner_index := (int(readBufferLen) - 1) - i
+				// skip if non char
+				if readBuffer[scanner_index] == '\x00' {
+					string_end_index--
+					continue
+				}
+				// append line sep if first read doesnt have it
+				if first_scan && readBuffer[scanner_index] != delim_char {
+					tempBuffer = append(tempBuffer, delim_char)
+				}
+				first_scan = false
 
-					out.Write(outputBuffer[0:])
-					wasOutput = true
-					outputBuffer = outputBuffer[:0]
-				} else {
-					if string_start_pos == searcher_pos {
-						out.Write(outputBuffer[0:])
-						wasOutput = true
-						firstOutput = false
+				//println("-->", string(readBuffer[scanner_index]))
+				// found delim
+				if readBuffer[scanner_index] == delim_char {
+					delim_index = int64(scanner_index)
+					string_start_index = delim_index + int64(1) //  \n[start from here][X][X][X]
+
+					// get the string behind this delim if within our readbuffer
+					if delim_index < readBufferLen-1 {
+						outputBuffer = append(outputBuffer, readBuffer[string_start_index:string_end_index+1]...)
 					}
-					if len(tempBuffer) > 0 {
-						// clear the temp buff but keep the underlying array
 
-						outputBuffer = append(outputBuffer, readBuffer[string_start_pos:string_end_pos]...)
+					// check if we have anything in temp buffer
+					if len(tempBuffer) > 0 {
+						// append to our output buffer
 						outputBuffer = append(outputBuffer, tempBuffer[0:]...)
 						tempBuffer = tempBuffer[:0]
-
-						out.Write(outputBuffer[0:])
-						wasOutput = true
-						firstOutput = false
-
-					} else {
-						outputBuffer = append(outputBuffer, readBuffer[string_start_pos:string_end_pos]...)
-						if firstOutput {
-							// for first read we might have excess nil bytes so need to do clean up
-							leng := len(outputBuffer) - 1
-							_ = leng
-							outputBuffer[len(outputBuffer)-1] = byte('\x00')
-							outputBuffer = bytes.Trim(outputBuffer, "\x00")
-							outputBuffer = append(outputBuffer, byte('\n'))
-							// outputBuffer[len(outputBuffer)] = byte('\n')
-						}
-						out.Write(outputBuffer[0:])
-						wasOutput = true
-						firstOutput = false
 					}
-					// clear the output buffer for the next row
+					out.Write(outputBuffer[0:])
 					outputBuffer = outputBuffer[:0]
+					string_end_index = delim_index
 				}
-			}
 
-			// end of scan
-			if searcher_pos == 0 {
-				string_start_pos = 0
-				// write to tempbuffer first because this is a partial row
-				//tempBuffer = append(tempBuffer, readBuffer[string_start_pos:string_end_pos]...)
-
-				s := make([]byte, len(readBuffer[string_start_pos:string_end_pos]))
-				copy(s[:], readBuffer[string_start_pos:string_end_pos])
-
-				tempBuffer = append(s, tempBuffer...)
-				if wasOutput {
-					tempBuffer = []byte{'\n'}
+				// end of scan without finding anything
+				if scanner_index == 0 {
+					// save everything in temp buffer
+					t := make([]byte, len(readBuffer[0:string_end_index+1]))
+					copy(t, readBuffer[0:string_end_index+1])
+					tempBuffer = append(t, tempBuffer...)
 				}
-				//reset our cursors
-				string_next_end_pos = 0
+
 			}
-			firstRead = false
+			// empty the read buffer
+			readBuffer = [MAX_LENGTH]byte{}
 		}
 	}
-	println("stop")
 }
--- gostan_test.go
+++ gostan_test.go
@@ -8,26 +8,53 @@ import (
 )
 
 func TestReadFromEnd(t *testing.T) {
-	filename := "./mockfile"
-	fr, err := os.Open(filename)
+	control_text := `===== Second file =====
+Fifth line - this is the last line
+Fourth line -gibberish gibberish gibberish gibberish 
+Third line -gibberish 
+Second line - gibberish gibberish 
+First line - this is the first line
+===== Second file =====
+===== First file =====
+Fifth line - this is the last line
+Fourth line -gibberish gibberish gibberish gibberish 
+Third line -gibberish 
+Second line - gibberish gibberish 
+First line - this is the first line
+===== First file =====
+`
+	filename1 := "./mockfile1"
+	fd1, err := os.Open(filename1)
 	if err != nil {
 		panic(fmt.Sprintf("open file error:%s", err.Error()))
 	}
-	defer fr.Close()
+	defer fd1.Close()
+
+	filename2 := "./mockfile2"
+	fd2, err := os.Open(filename2)
+	if err != nil {
+		panic(fmt.Sprintf("open file error:%s", err.Error()))
+	}
+	defer fd2.Close()
 
 	r, w := io.Pipe()
 
-	go ReadFromEnd(fr, w)
-	f, _ := os.Create("result")
+	go ReadFromEnd(w, fd1, fd2)
+
+	experiment_text := ""
 	for {
 		buff := make([]byte, 50)
 		n, err := r.Read(buff)
 		if n != 0 {
 			fmt.Print(string(buff[:n]))
-			f.Write(buff[:n])
+			experiment_text += string(buff[:n])
 		}
 		if err != nil {
 			break
 		}
 	}
+	if experiment_text != control_text {
+		t.Fail()
+	}
+	// println(result)
 }
